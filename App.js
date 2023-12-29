f058604cb6e4a2732a2ddf4b14fb410c23dc0135
lalit kashyap <lalit.kashyap.ug20@nsut.ac.in>
2023-12-30 00:29:20 +0530

    API Testing done

--- App.js
+++ App.js
@@ -3,6 +3,435 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css"; // Make sure to import your styles
+const RestaurantList= [
+    {
+    "info": {
+    "id": "417201",
+    "name": "Rominus Pizza And Burger",
+    "cloudinaryImageId": "9ec9ffd900c24ef751e2f34ba3d2fd4b",
+    "locality": "Laxmi Nagar",
+    "areaName": "Uttam Nagar",
+    "costForTwo": "₹150 for two",
+    "cuisines": [
+    "Pizzas",
+    "Italian",
+    "American",
+    "Snacks",
+    "Desserts",
+    "Beverages",
+    "Continental"
+    ],
+    "avgRating": 4.1,
+    "parentId": "8387",
+    "avgRatingString": "4.1",
+    "totalRatingsString": "1K+",
+    "sla": {
+    "deliveryTime": 25,
+    "lastMileTravel": 1.6,
+    "serviceability": "SERVICEABLE",
+    "slaString": "25 mins",
+    "lastMileTravelString": "1.6 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2023-12-30 04:00:00",
+    "opened": true
+    },
+    "badges": {},
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {},
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "₹125 OFF",
+    "subHeader": "ABOVE ₹249",
+    "discountTag": "FLAT DEAL"
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {}
+    },
+    "analytics": {},
+    "cta": {},
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "info": {
+    "id": "755546",
+    "name": "Bakingo",
+    "cloudinaryImageId": "0c53fd8f62e3d25ba56d1b4cfe572cef",
+    "locality": "Uttam Nagar",
+    "areaName": "DWARKA",
+    "costForTwo": "₹300 for two",
+    "cuisines": [
+    "Bakery",
+    "Desserts",
+    "Beverages",
+    "Snacks"
+    ],
+    "avgRating": 4.6,
+    "parentId": "3818",
+    "avgRatingString": "4.6",
+    "totalRatingsString": "100+",
+    "sla": {
+    "deliveryTime": 17,
+    "lastMileTravel": 1.6,
+    "serviceability": "SERVICEABLE",
+    "slaString": "17 mins",
+    "lastMileTravelString": "1.6 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2023-12-30 00:00:00",
+    "opened": true
+    },
+    "badges": {},
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {},
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "₹175 OFF",
+    "subHeader": "ABOVE ₹999",
+    "discountTag": "FLAT DEAL"
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "isNewlyOnboarded": true,
+    "restaurantOfferPresentationInfo": {}
+    },
+    "analytics": {
+    "context": "seo-data-84695cd1-de39-40ae-ac6d-d9e992f064f3"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/restaurants/bakingo-uttam-nagar-dwarka-delhi-755546",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "info": {
+    "id": "610732",
+    "name": "KFC",
+    "cloudinaryImageId": "f01666ac73626461d7455d9c24005cd4",
+    "locality": "Metro Station",
+    "areaName": "Dwarka More",
+    "costForTwo": "₹400 for two",
+    "cuisines": [
+    "Burgers",
+    "Biryani",
+    "American",
+    "Snacks",
+    "Fast Food"
+    ],
+    "avgRating": 4.2,
+    "parentId": "547",
+    "avgRatingString": "4.2",
+    "totalRatingsString": "1K+",
+    "sla": {
+    "deliveryTime": 15,
+    "lastMileTravel": 0.8,
+    "serviceability": "SERVICEABLE",
+    "slaString": "15 mins",
+    "lastMileTravelString": "0.8 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2023-12-30 04:00:00",
+    "opened": true
+    },
+    "badges": {},
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {},
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "EVERY ITEM",
+    "subHeader": "@ ₹179"
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {}
+    },
+    "analytics": {
+    "context": "seo-data-84695cd1-de39-40ae-ac6d-d9e992f064f3"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/restaurants/kfc-metro-station-dwarka-more-delhi-610732",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "info": {
+    "id": "22739",
+    "name": "Pizza Hut",
+    "cloudinaryImageId": "2b4f62d606d1b2bfba9ba9e5386fabb7",
+    "locality": "Bhagwati Garden",
+    "areaName": "Uttam Nagar",
+    "costForTwo": "₹350 for two",
+    "cuisines": [
+    "Pizzas"
+    ],
+    "avgRating": 4,
+    "parentId": "721",
+    "avgRatingString": "4.0",
+    "totalRatingsString": "5K+",
+    "sla": {
+    "deliveryTime": 26,
+    "lastMileTravel": 1.6,
+    "serviceability": "SERVICEABLE",
+    "slaString": "26 mins",
+    "lastMileTravelString": "1.6 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2023-12-30 01:00:00",
+    "opened": true
+    },
+    "badges": {},
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {},
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "60% OFF",
+    "subHeader": "UPTO ₹120"
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {}
+    },
+    "analytics": {
+    "context": "seo-data-84695cd1-de39-40ae-ac6d-d9e992f064f3"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/restaurants/pizza-hut-bhagwati-garden-uttam-nagar-delhi-22739",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "info": {
+    "id": "411519",
+    "name": "McDonald's",
+    "cloudinaryImageId": "6dc3ed2ca21d71acff7c2a51dfe4c720",
+    "locality": "Sector 14",
+    "areaName": "Dwarka",
+    "costForTwo": "₹400 for two",
+    "cuisines": [
+    "American"
+    ],
+    "avgRating": 4.3,
+    "parentId": "630",
+    "avgRatingString": "4.3",
+    "totalRatingsString": "5K+",
+    "sla": {
+    "deliveryTime": 27,
+    "lastMileTravel": 3.5,
+    "serviceability": "SERVICEABLE",
+    "slaString": "27 mins",
+    "lastMileTravelString": "3.5 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2023-12-29 23:59:00",
+    "opened": true
+    },
+    "badges": {},
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {},
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "10% OFF",
+    "subHeader": "UPTO ₹40"
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {}
+    },
+    "analytics": {
+    "context": "seo-data-84695cd1-de39-40ae-ac6d-d9e992f064f3"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/restaurants/mcdonalds-sector-14-dwarka-delhi-411519",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "info": {
+    "id": "514905",
+    "name": "Domino's Pizza",
+    "cloudinaryImageId": "etd7xk3euwfgkznwzlsg",
+    "locality": "Najafgarh Road",
+    "areaName": "Uttam Nagar",
+    "costForTwo": "₹400 for two",
+    "cuisines": [
+    "Pizzas",
+    "Italian",
+    "Pastas",
+    "Desserts"
+    ],
+    "avgRating": 4.4,
+    "parentId": "2456",
+    "avgRatingString": "4.4",
+    "totalRatingsString": "500+",
+    "sla": {
+    "deliveryTime": 25,
+    "serviceability": "SERVICEABLE",
+    "slaString": "25 mins",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2023-12-30 01:59:00",
+    "opened": true
+    },
+    "badges": {},
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {},
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "₹150 OFF",
+    "subHeader": "ABOVE ₹299",
+    "discountTag": "FLAT DEAL"
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {}
+    },
+    "analytics": {
+    "context": "seo-data-84695cd1-de39-40ae-ac6d-d9e992f064f3"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/restaurants/dominos-pizza-najafgarh-road-uttam-nagar-delhi-514905",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    }
+]
+
 
 const Header = () => {
   return (
@@ -22,47 +451,79 @@ const Header = () => {
   );
 };
 
-const RestaurantCard = () => {
-  return (
-    <div className="res-card">
-      <div className="food-image">
-        <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/f01666ac73626461d7455d9c24005cd4" alt="Food" />
-      </div>
-      <h1 className="food-heading">KFC</h1>
-      <div className="details">
-        <p>Rating: 4</p>
-        <p>Delivery Time: 22 mins</p>
-        <p>Cuisine: Fast Food</p>
-      </div>
-    </div>
-  );
-};
-const RestaurantList=()=>{
-    return(
-        <div className="res-container">
+// const RestaurantCard = (props) => {
+    
+//     console.log(data);
+//     const {name,cloudinaryImageId}=props?.info;
+//     console.log(name);
+//   return (
+//     <div className="res-card">
+//       <div className="food-image">
+//         <img src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+cloudinaryImageId} alt="Food" />
+//       </div>
+//       <h1 className="food-heading">{name}</h1>
+//       <div className="details">
+//         <p>Rating: 4</p>
+//         <p>Delivery Time: 22 mins</p>
+//         <p>Cuisine: Fast Food</p>
+//       </div>
+//     </div>
+//   );
+// };
 
-<RestaurantCard/>
-<RestaurantCard/>
-<RestaurantCard/>
-<RestaurantCard/>
-<RestaurantCard/>
-<RestaurantCard/>
-<RestaurantCard/>
-<RestaurantCard/>
-<RestaurantCard/>
-<RestaurantCard/>
+// const Body = () => {
+//   return (
+//     <div className="body">
+//       <div className="search">Search</div>
+//       <div className="res-list">
+      
+//        {
+//         RestaurantList.map((restaurant)=>{
+//             console.log(restaurant);
+//             <RestaurantCard data={restaurant}/>
+//         })
+//        }
 
+    
+       
+      
+
+
+//       </div>
+//     </div>
+//   );
+// };
+
+const RestaurantCard = (props) => {
+    const { name, cloudinaryImageId ,avgRating} = props?.data.info;
+    console.log(name);
+    return (
+        <div className="res-card">
+            <div className="food-image">
+                <img src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" + cloudinaryImageId} alt="Food" />
+            </div>
+            <h1 className="food-heading">{name}</h1>
+            <div className="details">
+                <p>{avgRating}</p>
+                <p>Delivery Time: 22 mins</p>
+                <p>Cuisine: Fast Food</p>
+            </div>
         </div>
-    )
-}
+    );
+};
 
 const Body = () => {
-  return (
-    <div className="body">
-      <div className="search">Search</div>
-      <RestaurantList/>
-    </div>
-  );
+    return (
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-list">
+                {RestaurantList.map((restaurant,index) => {
+                    console.log(restaurant);
+                    return <RestaurantCard key={restaurant.info.id} data={restaurant} />;
+                })}
+            </div>
+        </div>
+    );
 };
 
 const AppLayout = () => {
